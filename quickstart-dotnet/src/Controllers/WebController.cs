2c3855837b97e247b12003f068b79fba10a92f6e
SkullPsg <vishnusankar.sakthivel@gmail.com>
2018-02-16 14:55:58 +0530

    initial commit

---
+++ quickstart-dotnet/src/Controllers/WebController.cs
@@ -0,0 +1,103 @@
+﻿namespace todo.Controllers
+{
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Models;
+
+    public class WebController : Controller
+    {
+        [ActionName("Index")]
+        public async Task<ActionResult> IndexAsync()
+        {
+            var items = await DocumentDBRepository<WebsiteProperties>.GetItemsAsync();
+            return View(items);
+        }
+
+#pragma warning disable 1998
+        [ActionName("Create")]
+        public async Task<ActionResult> CreateAsync()
+        {
+            return View();
+        }
+#pragma warning restore 1998
+
+        [HttpPost]
+        [ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(WebsiteProperties item)
+        {
+            if (ModelState.IsValid)
+            {
+                await DocumentDBRepository<WebsiteProperties>.CreateItemAsync(item);
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(WebsiteProperties item)
+        {
+            if (ModelState.IsValid)
+            {
+                await DocumentDBRepository<WebsiteProperties>.UpdateItemAsync(item.Id, item);
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        [ActionName("Edit")]
+        public async Task<ActionResult> EditAsync(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            WebsiteProperties item = await DocumentDBRepository<WebsiteProperties>.GetItemAsync(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+
+        [ActionName("Delete")]
+        public async Task<ActionResult> DeleteAsync(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            WebsiteProperties item = await DocumentDBRepository<WebsiteProperties>.GetItemAsync(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmedAsync([Bind(Include = "Id")] string id)
+        {
+            await DocumentDBRepository<WebsiteProperties>.DeleteItemAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        [ActionName("Details")]
+        public async Task<ActionResult> DetailsAsync(string id)
+        {
+            WebsiteProperties item = await DocumentDBRepository<WebsiteProperties>.GetItemAsync(id);
+            return View(item);
+        }
+    }
+}
---
+++ quickstart-dotnet/src/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿namespace todo.Models
+{
+    using Microsoft.Azure.Documents;
+    using Newtonsoft.Json;
+
+    public class WebsiteProperties
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "isphishing")]
+        public bool isphishing { get; set; }
+
+        [JsonProperty(PropertyName = "rating")]
+        public int rating { get; set; }
+        
+    }
+}
